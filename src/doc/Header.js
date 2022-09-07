02a0feae7fc8ff9bc8dac9d3f5cbea9b1fc2d944
chae <corkdud@naver.com>
2022-09-07 17:49:02 +0900

    220907

--- src/doc/Header.js
+++ src/doc/Header.js
@@ -2,15 +2,61 @@ const MENU = [
     {
         id: 1,
         content: "menu01",
+        link: "/1",
+        submenu: [
+            {
+                content: "smenu01",
+                link: '/1'
+            },
+            {
+                content: "smenu01",
+                link: '/1'
+            },
+            {
+                content: "smenu01",
+                link: '/1'
+            },
+        ]
     },
     {
         id: 2,
         content: "menu02",
+        link: "/2",
+        submenu: [
+            {
+                content: "smenu02",
+                link: '/1'
+            },
+            {
+                content: "smenu02",
+                link: '/1'
+            },
+            {
+                content: "smenu02",
+                link: '/1'
+            },
+        ]
     },
     {
         id: 3,
         content: "menu03",
+        link: "/3",
+        submenu: [
+            {
+                content: "smenu03",
+                link: '/1'
+            },
+            {
+                content: "smenu03",
+                link: '/1'
+            },
+            {
+                content: "smenu03",
+                link: '/1'
+            },
+        ]
     },
+
 ];
 
 const Header = () => {
@@ -20,9 +66,11 @@ const Header = () => {
                 <a href="">LOGO</a>
             </h1>
             <nav className="GNB">
-                {/* <ul>{MENU.map((it, idx)=>(
+                <ul>{MENU.map((it, idx)=>(
+                    <li>
 
-                ))}</ul> */}
+                    </li>
+                ))}</ul>
             </nav>
         </header>
     );
